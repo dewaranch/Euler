e50b402b7b2bc4e0a7293e09de2f125b9a43b5c9
dewaranch <dewaranch@gmail.com>
2016-11-05 11:25:44 -0400

    changes

--- euler project.py
+++ euler project.py
@@ -1,6 +1,10 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
-## Euler Project
+
+## Euler Project 1
+
+sum=0
+def ThrFiSum():
+	for i in (1, 1000):
+   	if i%3==0 or i%5==0:
+      sum= sum + i
+  print(sum)
+      
