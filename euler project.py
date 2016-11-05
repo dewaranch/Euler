47efb284d7709f226bd3f1a726423d0f9b179571
dewaranch <dewaranch@gmail.com>
2016-11-05 11:32:22 -0400

    Update euler project.py

--- euler project.py
+++ euler project.py
@@ -1,10 +1,10 @@
 
 ## Euler Project 1
 
-sum=0
+sum = 0
 def ThrFiSum():
-	for i in (1, 1000):
-   	if i%3==0 or i%5==0:
-      sum= sum + i
-  print(sum)
+ for i in (1, 1000):
+  if i%3==0 or i%5==0:
+   sum= sum + i
+ print(sum)
       
